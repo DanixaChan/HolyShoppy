89830cdee8e456fa2bbcb98025e87a21ed85b73d
Danitsa Ch <dani.chandia@duocuc.cl>
2023-06-29 00:12:25 -0400

    Se agrega archivo Proyecto con Django 3.0 FV

---
+++ VERSION3.0/Danitsa/Proyecto con Django 3.0 FV/HolyShoppy/static/js/scripts.js
@@ -0,0 +1,37 @@
+// Funcionalidad de 'sidenav'
+// $(document).ready(function () {
+//   $(".sidenav").sidenav();
+// });
+$(document).ready(function () {
+  // Cierra el menú al hacer clic en un enlace
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+// Reinicio de la animación fa-bounce (rebote del carro)
+let carrito = document.getElementById("carro");
+
+carrito.addEventListener("mousemove", function () {
+  $(".fa-bounce", this).on("mouseleave", function () {
+    $(this).removeClass("fa-bounce");
+  });
+  $(".fa-bounce", this).on("mouseenter", function () {
+    $(this).addClass("fa-bounce");
+  });
+});
+// Reinicio de la animación fa-bounce (rebote del carro)
+let bolsita = document.getElementById("bolsita");
+
+bolsita.addEventListener("mousemove", function () {
+  $(".fa-bounce", this).on("mouseleave", function () {
+    $(this).removeClass("fa-bounce");
+  });
+  $(".fa-bounce", this).on("mouseenter", function () {
+    $(this).addClass("fa-bounce");
+  });
+});
+
+//Redireccion de página
+function irRegistro() {
+  location.href = "./registro.html";
+}
